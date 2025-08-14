dc8325644d6766f98606135b95a53d0964ed185f
Jonathan Ward <jon_lad@hotmail.com>
2025-08-14 11:26:29 +0100

    updated after install

--- codedeploy-scripts/after-install.sh
+++ codedeploy-scripts/after-install.sh
@@ -11,4 +11,4 @@ s3bucket=valegreo-acebook-bucket
 sudo chown -R ec2-user:ec2-user $repodir
 sudo chmod 755 $repodir/codedeploy-scripts/*.sh
 
-sudo socat TCP-LISTEN:80,fork TCP:127.0.0.1:3000
+sudo socat TCP-LISTEN:80,fork TCP:127.0.0.1:3000 1>/dev/null 2>&1 &
